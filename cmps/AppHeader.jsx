440b0bed6c68b048b1d600c35300adbed946020b
Ben Zion Fischer <benzionfisher@gmail.com>
2025-02-06 12:15:42 +0200

    customize AppHeader for books

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -5,11 +5,11 @@ export function AppHeader() {
     return (
         <header className="app-header full main-layout">
             <section>
-                <h1>React Car App</h1>
+                <h1>React Book App</h1>
                 <nav className="app-nav">
                     <NavLink to="/home" >Home</NavLink>
                     <NavLink to="/about"  >About</NavLink>
-                    <NavLink to="/car" >Cars</NavLink>
+                    <NavLink to="/book" >Books</NavLink>
                 </nav>
             </section>
         </header>
