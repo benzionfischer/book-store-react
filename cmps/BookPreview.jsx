c32e78f6508097e70819fe7074097c85fa60980e
Ben Zion Fischer <benzionfisher@gmail.com>
2025-02-08 12:06:02 +0200

    Improve book preview important fields

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -3,14 +3,10 @@ export function BookPreview({ book }) {
 
     return (
         <article className="car-preview">
-            <h2>{book.title}</h2>
-            <h4>Amount: {book.listPrice.amount}</h4>
-            <h4>Currency code: {book.listPrice.currencyCode}</h4>
-            <h4>Is on sale: {book.listPrice.isOnSale ? "Yes" : "No"}</h4>
-
-
-            {/* <h4>Book Speed: {book.speed}</h4> */}
-            {/* <img src={`../assets/img/${book.vendor}.png`} alt="car-image" /> */}
+            <h2>Title: {book.title}</h2>
+            <img src={`${book.thumbnail}`} alt="car-image" />
+            <h4>Categories: {book.categories}</h4>
+            <h4>Price: {book.listPrice.amount} {book.listPrice.currencyCode}</h4>
         </article>
     )
 }
